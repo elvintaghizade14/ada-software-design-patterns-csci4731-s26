8f1d7929f432317393201ba9adedbc4ec318c9cf
Hasanov070 <142946594+Hasanov070@users.noreply.github.com>
2026-02-03 20:21:04 +0400

    Thread-Safe Singleton Implementation

--- src/main/java/az/edu/ada/modules/module02/lecture04/LibraryApp.java
+++ src/main/java/az/edu/ada/modules/module02/lecture04/LibraryApp.java
@@ -1,41 +1,30 @@
-//package az.edu.ada.modules.module02.lecture04;
-//
-//public class LibraryApp {
-//
-//    public void saveBook() {
-//        // fetch from db: 1
-//
-//        // do internal process
-//
-//        // save into db: 2
-//    }
-//
-//    public static void main(String[] args) {
-//        DBConnection connection1 = DBConnection.getInstance();
-//
-//        connection1 = null; //???
-//
-//        System.out.println("Did some work");
-//
-//        DBConnection connection2 = DBConnection.getInstance();
-//
-//
-//        System.out.println("Did more work");
-//    }
-//
-//    public class DBConnection {
-//
-//        private static final DBConnection INSTANCE = new DBConnection();
-//
-//        private DBConnection() {
-//            System.out.println("Connected to the DB!");
-//        }
-//
-//        public static DBConnection getInstance() {
-//            if (INSTANCE == null) {
-//                INSTANCE = new DBConnection();
-//            }
-//            return INSTANCE;
-//        }
-//    }
-//}
+package az.edu.ada.modules.module02.lecture04;
+
+public class LibraryApp {
+
+    public void saveBook() {
+        DBConnection connection = DBConnection.getInstance();
+
+        // fetch from db
+        System.out.println("Fetching book...");
+
+        // process
+        System.out.println("Processing...");
+
+        // save
+        System.out.println("Saving...");
+    }
+
+    public static void main(String[] args) {
+
+        DBConnection connection1 = DBConnection.getInstance();
+
+        connection1 = null; // doesn't destroy singleton
+
+        System.out.println("Did some work");
+
+        DBConnection connection2 = DBConnection.getInstance();
+
+        System.out.println("Did more work");
+    }
+}
