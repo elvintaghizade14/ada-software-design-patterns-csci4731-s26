a6b159a17b9a43946dd2e585b4ec2719bf2519ec
Elvin Taghizade <elvintaghiyev184@gmail.com>
2026-04-14 08:29:23 +0400

    lesson1 class code attached

---
+++ spring-mvc-demo/src/main/java/az/edu/ada/springmvcdemo/SpringMvcDemoApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.springmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcDemoApplication.class, args);
+    }
+
+}
---
+++ spring-mvc-demo/src/test/java/az/edu/ada/springmvcdemo/SpringMvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.springmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
